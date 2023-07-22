49ee2bf401ace94f8e2fff2cf107ae3dc6809025
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-22 16:39:10 -0500

    10 users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Create a new view for User objects that handles all default RestFul API """
+
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
